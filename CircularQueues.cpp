95d8ca53c3db18b6714712479649dac53b98f92a
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-12 18:52:21 +0700

    memanggil dan menjalankan menggunakan int main

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -117,8 +117,62 @@ public :
             }
             cout << endl;
         }
-        
-        
+
     }
 
+};
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement Insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter you choice (1-4) : ";
+            cin>> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1' :
+            {
+                q.insert();
+                break;
+            }
+            case '2' :
+            {
+                q.remove();
+                break;
+            }
+            case '3' :
+            {
+                q.display();
+                break;
+            }
+            case '4' :
+            {
+                return 0;
+            }
+            default :
+            {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+    
+    return 0;
 }
