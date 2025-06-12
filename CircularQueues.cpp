c9e72e5ffcf39c6a4d7e9ab8557fc7f62cf57b8b
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-12 17:42:21 +0700

    membuat perulangan front

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -25,9 +25,17 @@ public :
         //1. Cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1))
         {
-            
+            cout << "\nQueue overflow\n";   // 2.b
+            return;                         // 1.b
         }
         
+        // 2. Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0; // 2.a
+            REAR = 0; // 2.B
+        }
+        else
     }
 
 }
