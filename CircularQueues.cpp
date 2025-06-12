bc2fcf6baba66f8e14a3c4a4666c283783431b2e
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-12 17:39:36 +0700

    membuat perulangan

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -21,6 +21,13 @@ public :
         cout << "Enter a number : ";
         cin >> num;
         cout << endl;
+
+        //1. Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1))
+        {
+            
+        }
+        
     }
 
 }
